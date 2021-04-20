72ef631ef53547dab38d2f752ebfdd29982637e5
Lucas Pontes <48536745+LucasDev13@users.noreply.github.com>
2021-04-20 19:47:37 -0300

    Delete user-api directory

--- user-api/src/main/resources/db/migration/V1__create_user_table.sql
+++
@@ -1,9 +0,0 @@
-create table users.user(
-    id bigserial primary key,
-    nome varchar(100) not null,
-    cpf varchar(100) not null,
-    endereco varchar(100) not null,
-    email varchar(100) not null,
-    telefone varchar(100) not null,
-    data_cadastro timestamp not null
-);
